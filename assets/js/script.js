6f189e87fdfa0d0bc3ac41d00bc36aebefb1b468
Belkbh <belkbh@gmail.com>
2022-08-25 16:42:25 -0400

    Got colors working with hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,10 @@ var orderedListEl = $('#ordered-list');
 
 orderedListEl.on('click', '.save-btn', SaveClicked);
 
+var grayColor = "#D1D1D1";
+var redColor = "#E28C8C";
+var greenColor = "#83EA8A";  
+
 function SaveClicked(event){
     var clickedThing = $(event.target).parent().parent().parent().children().eq(1).children().eq(0);
     var dataId = clickedThing.attr('data-id');
@@ -12,12 +16,14 @@ function SaveClicked(event){
     
 }
 
-function GiveValue(index){
-    orderedListEl.children().eq(index).children().eq(1).children().eq(0).val("Hello world");
-}
+// function GiveValue(index){
+//     orderedListEl.children().eq(index).children().eq(1).children().eq(0).val("Hello world");
+// }
 
-GiveValue(9);
+// GiveValue(9);
+var currentDay = $('#currentDay').html("" + moment().format("dddd, MMMM Do"));
 
+var getHour = moment().hour();
 
 var savedStringArray = [];
 // for(var i = 0; i < )
@@ -25,10 +31,11 @@ var savedStringArray = [];
 $('.inputArea').each(function(){
     var parentId = ($(this).attr('data-id'));
     var name = localStorage.getItem(parentId);
-    console.log("GOT ID: " + parentId + " GOT NAME: " + name + " THIS IS: " + this);
+    // console.log("GOT ID: " + parentId + " GOT NAME: " + name + " THIS IS: " + this);
     $(this).val(name);
+
+    if(parentId < getHour){$(this).css("background-color", grayColor);}
+    if(parentId == getHour){$(this).css("background-color", redColor);}
+    if(parentId > getHour){$(this).css("background-color", greenColor);}
 });
 
-var currentDay = $('#currentDay').html("" + moment().format("dddd, MMMM Do"));
-
-var getHour = moment().hour();
