ff0724c49cf65f94f22c78cbbc40431bf38e97fc
Belkbh <belkbh@gmail.com>
2022-08-25 17:20:32 -0400

    Added image to README, minor css changes

--- README.md
+++ README.md
@@ -5,9 +5,9 @@
 2. [Visuals](#visuals)
 
 ## Description
-This is a work schedule that is dynamically updated HTML CSS webpage powered by jQuery. It uses the web API Moment.js so tell the current time to show which tasks are passed, which are current, and which are in the future.
+This is a work schedule that is dynamically updated HTML CSS webpage powered by jQuery. It uses the web API Moment.js to tell the current time to show which tasks are in the past, which are current, and which are in the future. Users can type into each row, and save their inputs by clicking the save icons. Inputs reload with page.
 
 [Link to live site](https://bbelk.github.io/work-scheduler-project/) 
 
 ## Visuals
-![Alt text](./assets/images/HSO-Screenshot.png "Website Screenshot")
+![Alt text](./assets/images/Work-Day-Scheduler-Image.png "Website Screenshot")
--- assets/js/script.js
+++ assets/js/script.js
@@ -9,29 +9,21 @@ var greenColor = "#83EA8A";
 function SaveClicked(event){
     var clickedThing = $(event.target).parent().parent().parent().children().eq(1).children().eq(0);
     var dataId = clickedThing.attr('data-id');
-    // console.log("DATA ID " + dataId);
     var newValue = clickedThing.val();
-    // console.log("VALUE IS " + newValue + " " + moment().hour());
     localStorage.setItem(dataId, newValue)
     
 }
 
-// function GiveValue(index){
-//     orderedListEl.children().eq(index).children().eq(1).children().eq(0).val("Hello world");
-// }
-
-// GiveValue(9);
 var currentDay = $('#currentDay').html("" + moment().format("dddd, MMMM Do"));
 
 var getHour = moment().hour();
+// var getHour = 12; testing
 
-var savedStringArray = [];
-// for(var i = 0; i < )
 
 $('.inputArea').each(function(){
     var parentId = ($(this).attr('data-id'));
     var name = localStorage.getItem(parentId);
-    // console.log("GOT ID: " + parentId + " GOT NAME: " + name + " THIS IS: " + this);
+
     $(this).val(name);
 
     if(parentId < getHour){$(this).css("background-color", grayColor);}
