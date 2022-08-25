0b6726c820999530547bda485ba9d32365421909
Belkbh <belkbh@gmail.com>
2022-08-25 16:10:34 -0400

    Saving and loading complete, next is making the backgrounds change color depending on hour

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,16 +3,32 @@ var orderedListEl = $('#ordered-list');
 orderedListEl.on('click', '.save-btn', SaveClicked);
 
 function SaveClicked(event){
-    var parent = $(event.target).parent().parent().parent().children().eq(1).children().eq(0).val();
-    console.log("VALUE IS " + parent);
+    var clickedThing = $(event.target).parent().parent().parent().children().eq(1).children().eq(0);
+    var dataId = clickedThing.attr('data-id');
+    // console.log("DATA ID " + dataId);
+    var newValue = clickedThing.val();
+    // console.log("VALUE IS " + newValue + " " + moment().hour());
+    localStorage.setItem(dataId, newValue)
+    
 }
 
 function GiveValue(index){
     orderedListEl.children().eq(index).children().eq(1).children().eq(0).val("Hello world");
 }
 
-// GiveValue(2);
+GiveValue(9);
 
 
 var savedStringArray = [];
-for(var i = 0; i < )
+// for(var i = 0; i < )
+
+$('.inputArea').each(function(){
+    var parentId = ($(this).attr('data-id'));
+    var name = localStorage.getItem(parentId);
+    console.log("GOT ID: " + parentId + " GOT NAME: " + name + " THIS IS: " + this);
+    $(this).val(name);
+});
+
+var currentDay = $('#currentDay').html("" + moment().format("dddd, MMMM Do"));
+
+var getHour = moment().hour();
